7af2199d998c56f531334522ec6a09a4de27a94b
Ben Mares <15216687+maresb@users.noreply.github.com>
2021-05-15 17:50:35 +0200

    Build boto3-stubs-essential

---
+++ recipe/test_essential.py
@@ -0,0 +1,9 @@
+"""Make sure that all "essential" services are present."""
+
+from mypy_boto3_builder.service_name import ServiceName
+from pkg_resources import require
+
+essential_service_names = ServiceName.ESSENTIAL
+
+for name in essential_service_names:
+    require("mypy_boto3_" + name)
